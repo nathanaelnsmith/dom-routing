c47dd5727583162497988b9194df12251bcce12b
Nathanael Smith <nathanaelnsmith@gmail.com>
2017-10-31 14:45:51 -0700

    update tabs to spaces

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 export default (routes) => {
-	return {
-		fire(func, funcname, args) {
-			funcname = (funcname === undefined) ? 'init' : funcname;
-			if (func !== '' && routes[func] && typeof routes[func][funcname] == 'function') {
-				routes[func][funcname](args);
-			}
-		},
-		load() {
-			var bodyId = document.body.id;
-			let Router = this;
-			// hit up common first.
-			Router.fire('common');
+  return {
+    fire(func, funcname, args) {
+      funcname = (funcname === undefined) ? 'init' : funcname;
+      if (func !== '' && routes[func] && typeof routes[func][funcname] == 'function') {
+        routes[func][funcname](args);
+      }
+    },
+    load() {
+      var bodyId = document.body.id;
+      let Router = this;
+      // hit up common first.
+      Router.fire('common');
 
-			// do all the classes too.
-			document.body.className.split(/\s+/).forEach(function(classnm) {
-				Router.fire(classnm);
-				Router.fire(classnm, bodyId);
-			});
+      // do all the classes too.
+      document.body.className.split(/\s+/).forEach(function(classnm) {
+        Router.fire(classnm);
+        Router.fire(classnm, bodyId);
+      });
 
-			Router.fire('common', 'finalize');
-		}
-	};
+      Router.fire('common', 'finalize');
+    }
+  };
 }
